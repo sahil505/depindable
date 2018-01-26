bd6507049dce20ce429e9480ea61f5ddb2347752
Sahil Khokhar <sahilkhokhar505@gmail.com>
2018-01-27 01:50:19 +0530

    reverse geo coder working

--- backup.js
+++ backup.js
@@ -23,3 +23,26 @@ function initMap(){
   });
 }
 </script>
+
+
+
+function geocodeLatLng(obj) {
+  var latlng = {lat: obj.lat, lng: obj.lng};
+  geocoder.geocode({'location': latlng}, function(results, status) {
+    if (status === 'OK') {
+      if (results[0]) {
+        map.setZoom(11);
+        var marker = new google.maps.Marker({
+          position: latlng,
+          map: map
+        });
+        infowindow.setContent(results[0].formatted_address);
+        infowindow.open(map, marker);
+      } else {
+        window.alert('No results found');
+      }
+    } else {
+      window.alert('Geocoder failed due to: ' + status);
+    }
+  });
+}
